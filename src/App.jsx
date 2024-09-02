d0bf24c0e07f7f1a15c305e92189d1a23f50474f
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-09-02 22:35:35 +0530

    first creation

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App(){
+  return(
+    <>
+    </>
+  )
+}
+
+// https://fonts.google.com/specimen/Sorts+Mill+Goudy
+// https://fonts.google.com/specimen/Open+Sans
+// https://fonts.google.com/specimen/Lora
+// https://fonts.google.com/specimen/Alex+Brush
