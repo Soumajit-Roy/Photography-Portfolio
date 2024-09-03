9e94727f74eb0841a8ee53e04493229787c9987d
Soumajit Roy <itssoumajit.alt@gmail.com>
2024-09-03 22:05:52 +0530

    basic structure done

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,3 @@ export default function App(){
   )
 }
 
-// https://fonts.google.com/specimen/Sorts+Mill+Goudy
-// https://fonts.google.com/specimen/Open+Sans
-// https://fonts.google.com/specimen/Lora
-// https://fonts.google.com/specimen/Alex+Brush
